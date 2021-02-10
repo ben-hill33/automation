89e178e479acaab5b276119ff7a9fe02e1821580
BEN <benjamin.hill28@gmail.com>
2021-02-10 15:06:32 -0800

    extracted emails to txt file

---
+++ emails.py
@@ -0,0 +1,48 @@
+import re
+import os
+
+file_to_read = './assets/potential-contacts.txt'
+file_to_write = 'email-extracted.txt'
+
+delimiter_in_file = [',', ';']
+
+
+def validate_email(str_email):
+    if re.match("(.*)@(.*).(.*)", str_email):
+        return True
+    return False
+
+
+def write_file(list_data):
+    file = open(file_to_write, 'w+')
+    str_data = ''
+    for item in list_data:
+        str_data = str_data + item + '\n'
+    file.write(str_data)
+
+
+list_email = []
+
+file = open(file_to_read, 'r')
+
+list_line = file.readlines()
+
+for item_line in list_line:
+    item = str(item_line)
+    for delimiter in delimiter_in_file:
+        item = item.replace(str(delimiter), ' ')
+    word_list = item.split()
+    for word in word_list:
+        if(validate_email(word)):
+            list_email.append(word)
+
+if list_email:
+    unique_email = set(list_email)
+    print(len(unique_email), 'emails collected!')
+    write_file(unique_email)
+else:
+    print("No email found.")
+
+
+if __name__ == "__main__":
+    pass
